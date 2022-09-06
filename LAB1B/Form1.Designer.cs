9245e41dd360147118e8c04633b2ab0888d0d8b4
Alisa <alisa.momotova@mail.ru>
2022-09-06 13:14:12 +0300

    first commit

---
+++ LAB1B/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace LAB1B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(116, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 251);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите значение х:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(307, 450);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 2;
+            this.label2.MouseEnter += new System.EventHandler(this.label2_MouseEnter);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 339);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 35);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(385, 303);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 145);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите способ ввода";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(55, 96);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 26);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Рандом";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(55, 54);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(175, 26);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Самостоятельно";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 54);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(687, 527);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Задание 1. Вычисление по формулам";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LAB1B/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB1B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = 0;
+            Random a = new Random();
+
+            if (radioButton1.Checked)
+            {
+                    try
+                    {
+                        x = Convert.ToDouble(textBox1.Text);
+                    }
+                    catch
+                    {
+                    MessageBox.Show("Неверный формат числа       ");
+                    textBox1.Clear();
+                    textBox1.Focus();
+                    }
+                }
+
+            if (radioButton2.Checked)
+            {
+                x = a.Next(4, 1000);
+                label2.Text = "x = " + x.ToString("F3");
+             }
+
+            if ((x > -3) && (x <= 3))   //ОДЗ
+            {
+                MessageBox.Show("Число x должно принадлежать диапазону (-беск;-3]и(3;+беск)");
+            }
+
+            // первый результат
+            double z1 = (Math.Pow(x, 2) + (2 * x) - 3 + (x + 1) * Math.Sqrt(Math.Pow(x, 2) - 9)) / (Math.Pow(x, 2) - (2 * x) - 3 + (x - 1) * Math.Sqrt(Math.Pow(x, 2) - 9));
+                // второй результат
+                double z2 = Math.Sqrt(x + 3) / Math.Sqrt(x - 3);
+                // вывод ответа
+                label2.Text = "x = " + x.ToString("F3") + "\nz1 = " + z1.ToString("F3") + "\nz2 = " + z2.ToString("F3");
+            
+        }
+        // смена цвета фона надписи при наведении мыши на метку
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            label2.BackColor = Color.White;
+        }
+
+        // возвращение цвета фона метки после при покидания метки мышью
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.BackColor = Color.Transparent;
+        }
+
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                //блокировка кнопки на случай, если поля пустые или содержат лишь '-' и/или ','
+                if (textBox1.Text == "" || textBox1.Text == "-" || textBox1.Text == ","
+                    || textBox1.Text == "-," || textBox1.Text == " ")
+                    button1.Enabled = false;
+                else button1.Enabled = true;
+            }
+        }
+
+        // разрешение на ввод минуса (в начале числа), цифр 0-9, запятой и кнопки Backspace
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // запрет на ввод символов перед минусом
+            if ((sender as TextBox).Text.IndexOf('-') != -1 && (sender as
+                TextBox).SelectionStart == 0)
+            { e.KeyChar = '\0'; return; }
+
+            //разрешение ввода цифр
+            if (e.KeyChar >= '0' && e.KeyChar <= '9') return;
+
+            //разрешение ввода запятой
+            if (e.KeyChar == ',' && (sender as TextBox).Text.IndexOf(',') == -1) return;
+
+            //разрешение кнопки Backspace
+            if (e.KeyChar == (char)Keys.Back) return;
+
+            //разрешение ввода минуса в начале числа
+            if (e.KeyChar == '-' && (sender as TextBox).Text.IndexOf('-') == -1 &&
+                (sender as TextBox).SelectionStart == 0) return;
+
+            //запрет вввода других символов
+            e.KeyChar = '\0';
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton2.Checked = true;
+            button1.Enabled = true;
+            textBox1.Focus();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Если в поле texBox1 нет данных, сделать кнопку button1 недоступной
+            if (textBox1.Text.Length == 0)
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;          
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            button1.Enabled = true;
+        }
+    }
+
+}
